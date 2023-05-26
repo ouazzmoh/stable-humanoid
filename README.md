06ad84eca1b950c44a0008aca2c4a12963daa3c8
ouazzmoh <mohammed.ouazzani-chahdi@grenoble-inp.org>
2023-05-26 11:31:51 +0200

    Considering the limits

--- README.md
+++ README.md
@@ -3,4 +3,22 @@
 
 ## Simulation with no perturbations
 
-![Example Image](./predictive_control.png)
+![Example Image](plots/predictive_control.png)
+
+## Continuous perturbation of 1 m.s-2 (5 steps of time)
+
+![Example Image](plots/perturbations.png)
+
+## Instant perturbation of 6 m.s-2
+
+![Example Image](plots/instant_perturbation.png)
+
+## Considering the limits explicitely 
+
+![Example Image](plots/qp.png)
+
+## Adding perturbations to the qp resoltion method
+
+We use a OSQP solver from the python library qpsolvers
+
+![Example Image](plots/qp_perturbations.png)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import cvxpy as cp
+from qpsolvers import solve_qp
+import scipy.sparse as sp
 
 
 def p_u_matrix(t_step, h, g, n):
@@ -57,6 +60,20 @@ def optimal_jerk(t_step, h_com, g, n, xk_init, zk_ref, r_q):
     return result
 
 
+def optimal_jerk_qp(t_step, h_com, g, n, xk_init, zk_min, zk_max, r_q, Pu, Px):
+    # Objective matrix Q (Quadratic part)
+    Q = np.eye(n)
+    # Objective vector p (Linear part)
+    p = np.zeros(n)
+    # Inequality constraints Gx <= h
+    G = np.vstack((Pu, -Pu))  # double stack Pu to handle both upper and lower bound constraints
+    epsilon = 1e-3 * np.ones(n)  # using epsilon in the constraints to make the inequality strict
+    h = np.hstack((zk_max - Px @ xk_init - epsilon, -(zk_min - Px @ xk_init) - epsilon))  # same here for h
+    # Solving the QP problem
+    x = solve_qp(Q, p, G, h, solver="osqp")
+    return x
+
+
 def next_com(jerk, previous, t_step):
     """
     Getting the next (position, velocity, acceleration) vector using the recursive
@@ -81,29 +98,44 @@ def mid(arr): return (arr[0] + arr[1])/2
 
 
 
+def construct_zmin_zmax(steps):
+    """
+        Construct the minimum and maximum for the center of pressure
+        :param steps: number of steps to sample the horizon
+        :return: array of z_ref
+        """
+    # Constructing z_ref
+    # the convex hull when the support is in the left
+    z_left_single = np.array([-0.13, -0.07])
+    # z_left_ref = mid(z_left_single)
+    # the convex hull when the support is in the right
+    z_right_single = np.array([0.07, 0.13])
+    # z_right_ref = mid(z_right_single)
+    # the convex hull when the support is double
+    z_double = np.array([-0.13, 0.13])
+    # z_double_ref = mid(z_double)
+    # steps = 300
+    begin_min = [z_double[0]] * int(steps * 0.26)
+    begin_max = [z_double[1]] * int(steps * 0.26)
+    left_min = [z_left_single[0]] * int(steps * 0.07) + [z_double[0]] * int(steps * 0.01)
+    left_max = [z_left_single[1]] * int(steps * 0.07) + [z_double[1]] * int(steps * 0.01)
+    right_min = [z_right_single[0]] * int(steps * 0.07) + [z_double[0]] * int(steps * 0.01)
+    right_max = [z_right_single[1]] * int(steps * 0.07) + [z_double[1]] * int(steps * 0.01)
+    zk_min = begin_min + (left_min + right_min) * 3
+    zk_min += [z_double[0]] * abs((steps - len(zk_min)) % steps)
+    zk_max = begin_max + (left_max + right_max) * 3
+    zk_max += [z_double[1]] * abs((steps - len(zk_max)) % steps)
+    return np.array(zk_min), np.array(zk_max)
+
+
 def construct_zref(steps):
     """
     Construct the references for the center of pressure
     :param steps: number of steps to sample the horizon
     :return: array of z_ref
     """
-    # Constructing z_ref
-    # the convex hull when the support is in the left
-    z_left_single = np.array([-0.17, -0.04])
-    z_left_ref = mid(z_left_single)
-    # the convex hull when the support is in the right
-    z_right_single = np.array([0.04, 0.17])
-    z_right_ref = mid(z_right_single)
-    # the convex hull when the support is double
-    z_double = np.array([-0.17, 0.17])
-    z_double_ref = mid(z_double)
-    # steps = 300
-    begin = [z_double_ref] * int(steps * 0.26)
-    left = [z_left_ref] * int(steps * 0.07) + [z_double_ref] * int(steps * 0.01)
-    right = [z_right_ref] * int(steps * 0.07) + [z_double_ref] * int(steps * 0.01)
-    zk_ref = begin + (left + right) * 3
-    zk_ref += [z_double_ref] * abs((steps - len(zk_ref)) % steps)
-    return np.array(zk_ref)
+    zk_min, zk_max = construct_zmin_zmax(steps)
+    return (zk_min + zk_max)/2
 
 def simulation_no_feedback():
     steps = 300
@@ -161,18 +193,121 @@ def simulation_with_feedback():
 
 
 
+def simulation_with_feedback_qp():
+    t_step = 5e-3
+    # We simulate from 0 to 9 (s)
+    steps = int(9/t_step)
+    g = 9.81
+    h_com = 0.8
+    r_q = 1e-6
+    xk_init = (0, 0, 0)
+    zk_min, zk_max = construct_zmin_zmax(steps=steps)
+    com = []
+    com_velocity = []
+    com_acceleration = []
+    cop = []
+    prev = xk_init
+    window_steps = 300
+
+    # Construction of reused matrices for performance
+    Pu = p_u_matrix(t_step, h_com, g, window_steps)
+    Px = p_x_matrix(t_step, h_com, g, window_steps)
+    for i in range(steps - window_steps):
+        jerk = optimal_jerk_qp(t_step=t_step, h_com=h_com, g=g, n=window_steps, xk_init=prev,
+                               zk_min=zk_min[i:window_steps + i], zk_max=zk_max[i:window_steps + i],
+                               r_q=r_q, Pu=Pu, Px=Px)
+        next = next_com(jerk=jerk[0], previous=prev, t_step=t_step)
+        com.append(next[0])
+        com_velocity.append(next[1])
+        com_acceleration.append(next[2])
+        cop.append(np.array([1, 0, -h_com / g]) @ next)
+        prev = next
+    x = np.arange(0, 9, t_step)
+    return cop, com, com_velocity, com_acceleration, zk_min, zk_max, x
+
+
+def simulation_with_feedback_qp_perturbations():
+    t_step = 5e-3
+    # We simulate from 0 to 9 (s)
+    steps = int(9/t_step)
+    g = 9.81
+    h_com = 0.8
+    r_q = 1e-6
+    xk_init = (0, 0, 0)
+    zk_min, zk_max = construct_zmin_zmax(steps=steps)
+    com = []
+    com_velocity = []
+    com_acceleration = []
+    cop = []
+    prev = xk_init
+    window_steps = 300
+
+    # Construction of reused matrices for performance
+    Pu = p_u_matrix(t_step, h_com, g, window_steps)
+    Px = p_x_matrix(t_step, h_com, g, window_steps)
+    for i in range(steps - window_steps):
+        jerk = optimal_jerk_qp(t_step=t_step, h_com=h_com, g=g, n=window_steps, xk_init=prev,
+                               zk_min=zk_min[i:window_steps + i], zk_max=zk_max[i:window_steps + i],
+                               r_q=r_q, Pu=Pu, Px=Px)
+        next = next_com(jerk=jerk[0], previous=prev, t_step=t_step)
+        com.append(next[0])
+        com_velocity.append(next[1])
+        com_acceleration.append(next[2])
+        cop.append(np.array([1, 0, -h_com / g]) @ next)
+        # perturbation after (2.5 seconds)
+        if i == 500:
+            # 2.5 seconds we add an impact, it changes the acceleration
+            next[2] -= 10
+        prev = next
+    x = np.arange(0, 9, t_step)
+    return cop, com, com_velocity, com_acceleration, zk_min, zk_max, x
+def simulation_with_perturbations():
+    t_step = 5e-3
+    t_end = 9
+    # We simulate from 0 to 9 (s)
+    steps = int(t_end/t_step)
+    g = 9.81
+    h_com = 0.8
+    r_q = 1e-6
+    xk_init = (0, 0, 0)
+    zk_ref = construct_zref(steps=steps)
+    com = []
+    com_velocity = []
+    com_acceleration = []
+    cop = []
+    prev = xk_init
+    window_steps = 300
+    for i in range(steps - window_steps):
+        jerk = optimal_jerk(t_step=t_step, h_com=h_com, g=g, n=window_steps, xk_init=prev,
+                            zk_ref=zk_ref[i:window_steps + i], r_q=r_q)
+        next = next_com(jerk=jerk[0], previous=prev, t_step=t_step)
+        com.append(next[0])
+        com_velocity.append(next[1])
+        com_acceleration.append(next[2])
+        cop.append(np.array([1, 0, -h_com / g]) @ next)
+        # perturbation after (2.5 seconds)
+        if i == 500:
+            # 2.5 seconds we add an impact, it changes the acceleration
+            next[2] -= 10
+        prev = next
+    x = np.arange(0, 9, t_step)
+    return cop, com, com_velocity, com_acceleration, zk_ref, x
+
+
 
 def main():
-    cop, com, _, _, zk_ref, x = simulation_with_feedback()
+    cop, com, _, _, zk_min, zk_max, x = simulation_with_feedback_qp_perturbations()
     # x is used to show the proper scale in the x-axis
     x = x[:len(cop)]
-    plt.plot(x, zk_ref[:len(cop)], linestyle="--", color="blue")
-    plt.plot(x, cop, color="green")
-    plt.plot(x, com, color="red")
-    plt.ylim(-0.20, 0.20)
-    plt.legend(['z_ref', 'cop', 'com'])
-    plt.title("ZMP predictive control (R/Q = 1e-6)")
+    plt.plot(x, zk_min[:len(cop)], linestyle="--", color="blue")
+    plt.plot(x, zk_max[:len(cop)], linestyle="--", color="blue")
+    plt.plot(x, cop, color="green", label="cop")
+    plt.plot(x, com, color="red", label="com")
+    plt.ylim(-0.15, 0.15)
+    plt.legend()
+    plt.title("QP resolution")
     plt.show()
+
 
 
 
