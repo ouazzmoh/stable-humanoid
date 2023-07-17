df5b079a67e74a39c6b1c036c7b178f394efcd1a
Akram <akram.zaki@grenoble-inp.org>
2023-07-17 16:06:58 +0200

    Add docstrings

--- src/move_foot.py
+++ src/move_foot.py
@@ -5,18 +5,41 @@ from pink import solve_ik
 from loop_rate_limiters import RateLimiter
 from utils import get_control_points
 import numpy as np
+import pink
+from pink.tasks import Task, FrameTask
+from typing import List, Optional
+from com_task import ComTask
 
-def move_foot(configuration,
-              tasks,  
-              foot_task, 
-              com_task,
+def move_foot(configuration : pink.Configuration,
+              tasks: List[Task],  
+              foot_task:FrameTask , 
+              com_task: ComTask,
               curve: BezierCurve,
-              com_positions,
-              solver,
+              com_positions:np.ndarray,
+              solver:str,
               rate : RateLimiter,
-              viz: pin.visualize.MeshcatVisualizer=None,
-              display=True,
+              viz: Optional[pin.visualize.MeshcatVisualizer]=None,
+              display:Optional[bool]=True,
               **kwargs):
+    """
+    Move the foot along a Bezier curve while controlling the Center of Mass (COM)
+    and additional tasks.
+
+    Args:
+        configuration: Robot configuration.
+        tasks: List of tasks controlling the robot.
+        foot_task: Task controlling the foot.
+        com_task: Task controlling the Center of Mass (COM).
+        curve: Bezier curve defining the foot trajectory.
+        com_positions: Array of COM positions over time.
+        solver: Solver to use for inverse kinematics.
+        rate: Rate limiter controlling the visualization frame rate.
+        viz: Meshcat visualizer for visualization (default: None).
+        display: Flag indicating whether to display the robot configuration 
+                (default: True).
+        **kwargs: Additional tasks and their target values.
+
+    """
     t = 0.0
     dt = rate.period
     frequency = 1.0 / dt
@@ -45,9 +68,33 @@ def move_foot(configuration,
             rate.sleep()
             t += dt
             
-def get_foot_curve(src, dst, dz=.15):
+def get_foot_curve(src:np.ndarray, dst:np.ndarray, dz:Optional[float]=.15):
+    """
+    Compute a Bezier curve for the foot trajectory given the source
+    and destination positions.
+
+    Args:
+        src: Source position of the foot.
+        dst: Destination position of the foot.
+        dz : Vertical displacement of the curve (default: 0.15).
+
+    Returns:
+        BezierCurve: Bezier curve defining the foot trajectory.
+    """
     control_points_l = get_control_points(src, dst, dz=dz)
     return BezierCurve(control_points_l)
 
-def get_com_positions(src, dst, frequency=50.0):
+def get_com_positions(src:np.ndarray, dst:np.ndarray, frequency:Optional[float]=50.0):
+    """
+    Generate an array of center of mass positions between the source
+    and destination positions.
+
+    Args:
+        src: Source position of the center of mass.
+        dst: Destination position of the center of mass.
+        frequency: Number of positions to generate per unit of time (default: 50.0).
+
+    Returns:
+        np.ndarray: Array of center of mass positions.
+    """
     return np.linspace(src, dst, frequency)
