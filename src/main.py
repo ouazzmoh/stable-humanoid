9e77cde468e448c8018858e827cc1d302af9f62a
Akram <akram.zaki@grenoble-inp.org>
2023-07-18 14:09:19 +0200

    Minor changes

--- src/main.py
+++ src/main.py
@@ -24,34 +24,70 @@ try:
     from robot_descriptions.loaders.pinocchio import load_robot_description
 except ModuleNotFoundError:
     raise ModuleNotFoundError(
-        "Examples need robot_descriptions, "
-        "try `pip install robot_descriptions`"
+        "Examples need robot_descriptions, " "try `pip install robot_descriptions`"
     )
 
-robot = load_robot_description(
-        "jvrc_description", root_joint=pin.JointModelFreeFlyer()
-    )
+robot = load_robot_description("jvrc_description", root_joint=pin.JointModelFreeFlyer())
 # print(list(robot.model.names))
 # sys.exit()
 viz = pin.visualize.MeshcatVisualizer(
-        robot.model, robot.collision_model, robot.visual_model
-    )
+    robot.model, robot.collision_model, robot.visual_model
+)
 robot.setVisualizer(viz, init=False)
 viz.initViewer(open=True)
 viz.loadViewerModel()
-list_data = [ 8.29756125e-02, -9.10813747e-05,  8.69033893e-03,  7.94971455e-05,
-                -2.00299738e-03, -5.32537164e-06,  9.99997991e-01, -1.82130515e-02,
-                -7.96762498e-05, -2.81783003e-05,  3.98387475e-01, -3.01838539e-05,
-                -3.88145027e-01, -1.79387328e-02, -4.68154345e-05, -3.64688202e-05,
-                3.97755529e-01, -6.64565981e-05, -3.87904610e-01,  0.00000000e+00,
-                0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  0.00000000e+00,
-                0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  0.00000000e+00,
-                0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  0.00000000e+00,
-                0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  0.00000000e+00,
-                0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  0.00000000e+00,
-                0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  0.00000000e+00,
-                0.00000000e+00,  0.00000000e+00,  0.00000000e+00,  0.00000000e+00,
-                0.00000000e+00,  0.00000000e+00,  0.00000000e+00
+list_data = [
+    8.29756125e-02,
+    -9.10813747e-05,
+    8.69033893e-03,
+    7.94971455e-05,
+    -2.00299738e-03,
+    -5.32537164e-06,
+    9.99997991e-01,
+    -1.82130515e-02,
+    -7.96762498e-05,
+    -2.81783003e-05,
+    3.98387475e-01,
+    -3.01838539e-05,
+    -3.88145027e-01,
+    -1.79387328e-02,
+    -4.68154345e-05,
+    -3.64688202e-05,
+    3.97755529e-01,
+    -6.64565981e-05,
+    -3.87904610e-01,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
+    0.00000000e00,
 ]
 q_ref = np.array(list_data)
 robot.q0 = q_ref
@@ -59,23 +95,27 @@ configuration = pink.Configuration(robot.model, robot.data, robot.q0)
 viz.display(configuration.q)
 T_pred = 100e-3  # (s)
 T_control = 100e-3  # (s)
-simulation_time =10  # (s)
+simulation_time = 10  # (s)
 prediction_time = 2  # (s)
 g = 9.81
-h = pin.centerOfMass(configuration.model, configuration.data, configuration.q)[2] + 0.746  # (m)
+h = (
+    pin.centerOfMass(configuration.model, configuration.data, configuration.q)[2]
+    + 0.746
+)  # (m)
 foot_dimensions = [
-    0.225, 
-    np.abs(configuration.get_transform_frame_to_world("r_ankle").copy().translation[1])
-    ]  # length(x), width(y)
-spacing = (0.0, 
-           np.abs(configuration.get_transform_frame_to_world("r_ankle").copy().translation[1])
-           )  # lateral spacing between feet
-duration_double_init = 0.8  # (s)
-duration_step = 1  # (s)
+    0.225,
+    np.abs(configuration.get_transform_frame_to_world("r_ankle").copy().translation[1]),
+]  # length(x), width(y)
+spacing = (
+    0.0,
+    np.abs(configuration.get_transform_frame_to_world("r_ankle").copy().translation[1] + .6),
+)  # lateral spacing between feet
+duration_double_init = .8  # (s)
+duration_step = .8  # (s)
 steps = int(simulation_time / T_control)
 alpha = 1  # Weight for jerk
 gamma = 1e3  # Weight for zk_ref
-beta = 1   # Weight for velocity
+beta = 1  # Weight for velocity
 average_speed = (0.3, 0)
 stop_at = (8, 10)  # (s)
 
@@ -87,14 +127,22 @@ def move(trajectory_type, debug=False, store=False, perturbations=None):
     xk_init = (0, 0, 0)
     yk_init = (0, 0, 0)
     # Footstep planning
-    step_planner = FootstepPlanner(robot_mpc, simulation_time, duration_double_init,
-                                   duration_step, trajectory_type=trajectory_type, average_speed=average_speed,
-                                   stop_at=stop_at)
+    step_planner = FootstepPlanner(
+        robot_mpc,
+        simulation_time,
+        duration_double_init,
+        duration_step,
+        trajectory_type=trajectory_type,
+        average_speed=average_speed,
+        stop_at=stop_at,
+    )
 
-    zk_min_x, zk_max_x, zk_min_y, zk_max_y, theta_ref = step_planner.footsteps_to_array(0, simulation_time, T_control)
+    zk_min_x, zk_max_x, zk_min_y, zk_max_y, theta_ref = step_planner.footsteps_to_array(
+        0, simulation_time, T_control
+    )
     t = np.arange(0, simulation_time, T_control)
-    zk_ref_x = (zk_min_x + zk_max_x)/2
-    zk_ref_y = (zk_min_y + zk_max_y)/2
+    zk_ref_x = (zk_min_x + zk_max_x) / 2
+    zk_ref_y = (zk_min_y + zk_max_y) / 2
     plt.plot(t, zk_ref_x, label="zk_ref_x")
     plt.plot(t, zk_ref_y, label="zk_ref_y")
     plt.xlabel("time (s)")
@@ -103,10 +151,23 @@ def move(trajectory_type, debug=False, store=False, perturbations=None):
     plt.legend()
     plt.show()
 
-
     # Running the MPC
-    controller = MPC(simulation_time, prediction_time, T_control, T_pred, robot_mpc, step_planner,
-                     alpha, beta, gamma, xk_init, yk_init, write_hdf5=store, debug=debug, perturbations=perturbations)
+    controller = MPC(
+        simulation_time,
+        prediction_time,
+        T_control,
+        T_pred,
+        robot_mpc,
+        step_planner,
+        alpha,
+        beta,
+        gamma,
+        xk_init,
+        yk_init,
+        write_hdf5=store,
+        debug=debug,
+        perturbations=perturbations,
+    )
 
     # Initialize state of the robot
     robot_mpc.initialize_position(xk_init, yk_init, g)
@@ -122,7 +183,14 @@ def move(trajectory_type, debug=False, store=False, perturbations=None):
     for i in range(n_iterations):
         # print(vars(robot_mpc))
         # sys.exit()
-        curr_com, _, _, curr_cop, curr_left, curr_right = robot_mpc.get_positional_attributes()
+        (
+            curr_com,
+            _,
+            _,
+            curr_cop,
+            curr_left,
+            curr_right,
+        ) = robot_mpc.get_positional_attributes()
         com_x.append(curr_com[0])
         com_y.append(curr_com[1])
         cop_x.append(curr_cop[0])
@@ -143,10 +211,10 @@ def move(trajectory_type, debug=False, store=False, perturbations=None):
     left_indices = group_not_none(left_foot)
     right_indices = group_not_none(right_foot)
 
-    for k in range(len(left_indices)-1):
-        corresp_com_left.append(com[left_indices[k][1]:left_indices[k+1][0]])
-    for k in range(len(right_indices)-1):
-        corresp_com_right.append(com[right_indices[k][1]:right_indices[k+1][0]])
+    for k in range(len(left_indices) - 1):
+        corresp_com_left.append(com[left_indices[k][1] : left_indices[k + 1][0]])
+    for k in range(len(right_indices) - 1):
+        corresp_com_right.append(com[right_indices[k][1] : right_indices[k + 1][0]])
 
     left_foot_unique = [left_foot[i[0]] for i in left_indices]
     right_foot_unique = [right_foot[i[0]] for i in right_indices]
@@ -156,57 +224,42 @@ def move(trajectory_type, debug=False, store=False, perturbations=None):
     print("com left ->", corresp_com_left)
     print("com right -> ", corresp_com_right)
 
-    """
-    [(0.0, 0.325), (0.175, 0.325), (0.8749999999999999, 0.325), (1.575, 0.325), (2.2749999999999995, 0.325), (2.9749999999999996, 0.325)]
-    [(0.0, -0.325), (0.5249999999999999, -0.325), (1.2249999999999999, -0.325), (1.9249999999999998, -0.325), (2.625, -0.325), (3.325, -0.325)]
-    """
     # todo: Start with left
-    left_foot_task = FrameTask(
-        "l_ankle", position_cost=100.0, orientation_cost=1.0
-    )
+    left_foot_task = FrameTask("l_ankle", position_cost=100.0, orientation_cost=1.0)
 
     pelvis_task = FrameTask(
         "PELVIS_S", position_cost=[0.0, 0.0, 1.0], orientation_cost=3.0
     )
-    right_foot_task = FrameTask(
-        "r_ankle", position_cost=100.0, orientation_cost=1.0
-    )
+    right_foot_task = FrameTask("r_ankle", position_cost=100.0, orientation_cost=1.0)
     com_task = ComTask(position_cost=[1.0, 10.0, 1.0])
     posture_task = PostureTask(
-        cost=1e-1, # 1e-1
+        cost=1e-1,  # 1e-1
     )
     posture_task.set_target(robot.q0)
     tasks = [
-        left_foot_task, 
+        left_foot_task,
         posture_task,
         pelvis_task,
         right_foot_task,
         com_task,
-
-
     ]
 
     pelvis_task.set_target_from_configuration(configuration)
     com_task.set_target_from_configuration(configuration)
-    left_foot_task.set_target(
-        configuration.get_transform_frame_to_world("l_ankle")
-    )
+    left_foot_task.set_target(configuration.get_transform_frame_to_world("l_ankle"))
 
-
-    right_foot_task.set_target(
-        configuration.get_transform_frame_to_world("r_ankle")
-    )
+    right_foot_task.set_target(configuration.get_transform_frame_to_world("r_ankle"))
 
     # Select QP solver
     solver = qpsolvers.available_solvers[0]
     if "quadprog" in qpsolvers.available_solvers:
         solver = "quadprog"
     viewer = viz.viewer
-    meshcat_shapes.frame(viewer["r_ankle"], opacity=1.)
-    meshcat_shapes.frame(viewer["r_ankle_target"], opacity=.5)
-    meshcat_shapes.frame(viewer["l_ankle"], opacity=1.)
-    meshcat_shapes.frame(viewer["l_ankle_target"], opacity=.5)
-    meshcat_shapes.frame(viewer["com"], opacity=1., axis_length=1.)
+    meshcat_shapes.frame(viewer["r_ankle"], opacity=1.0)
+    meshcat_shapes.frame(viewer["r_ankle_target"], opacity=0.5)
+    meshcat_shapes.frame(viewer["l_ankle"], opacity=1.0)
+    meshcat_shapes.frame(viewer["l_ankle_target"], opacity=0.5)
+    meshcat_shapes.frame(viewer["com"], opacity=1.0, axis_length=1.0)
     rate = RateLimiter(frequency=50.0)
 
     src_r = configuration.get_transform_frame_to_world("r_ankle").copy()
@@ -214,27 +267,49 @@ def move(trajectory_type, debug=False, store=False, perturbations=None):
 
     src_l = configuration.get_transform_frame_to_world("l_ankle").copy()
     src_l = src_l.translation
-    for i in range(len(right_foot_unique)- 1):
-        #left_foot
+    for i in range(len(right_foot_unique) - 1):
+        # left_foot
         dst_l = np.array([*left_foot_unique[i + 1], src_l[2]])
-        curve_l = get_foot_curve(src_l, dst_l, dz=.15)
+        curve_l = get_foot_curve(src_l, dst_l, dz=0.15)
         com_l = get_com_positions(corresp_com_left[i][0], corresp_com_left[i][-1], 50)
-        move_foot(configuration, tasks, left_foot_task, com_task, curve_l, com_l, solver, rate, viz)
-        src_l = configuration.get_transform_frame_to_world(left_foot_task.body).translation
-        #right_foot
+        move_foot(
+            configuration,
+            tasks,
+            left_foot_task,
+            com_task,
+            curve_l,
+            com_l,
+            solver,
+            rate,
+            viz,
+        )
+        src_l = configuration.get_transform_frame_to_world(
+            left_foot_task.body
+        ).translation
+        # right_foot
         dst_r = np.array([*right_foot_unique[i + 1], src_r[2]])
-        curve_r = get_foot_curve(src_r, dst_r, dz=.15)
+        curve_r = get_foot_curve(src_r, dst_r, dz=0.15)
         com_r = get_com_positions(corresp_com_right[i][0], corresp_com_right[i][-1], 50)
-        move_foot(configuration, tasks, right_foot_task, com_task, curve_r, com_r, solver, rate, viz)
-        src_r = configuration.get_transform_frame_to_world(right_foot_task.body).translation
-        
+        move_foot(
+            configuration,
+            tasks,
+            right_foot_task,
+            com_task,
+            curve_r,
+            com_r,
+            solver,
+            rate,
+            viz,
+        )
+        src_r = configuration.get_transform_frame_to_world(
+            right_foot_task.body
+        ).translation
 
 
 def main():
-    trajectory_type = "forward"
+    trajectory_type = "upwards_turning"
     move(trajectory_type, debug=False)
 
 
 if __name__ == "__main__":
     main()
-
