fba8e29357e6298fdc914bd12abefca8ae972ef7
ouazzmoh <mohammed.ouazzani-chahdi@grenoble-inp.org>
2023-08-09 14:30:11 +0200

    Order of visualization fixed, updated with readme

--- src/main.py
+++ src/main.py
@@ -32,13 +32,13 @@ except ModuleNotFoundError:
         "Examples need robot_descriptions, " "try `pip install robot_descriptions`"
     )
 
+
+
 robot = load_robot_description("jvrc_description", root_joint=pin.JointModelFreeFlyer())
 viz = pin.visualize.MeshcatVisualizer(
     robot.model, robot.collision_model, robot.visual_model
 )
 robot.setVisualizer(viz, init=False)
-viz.initViewer(open=True)
-viz.loadViewerModel()
 list_data = [
     8.29756125e-02,
     -9.10813747e-05,
@@ -95,7 +95,7 @@ list_data = [
 q_ref = np.array(list_data)
 robot.q0 = q_ref
 configuration = pink.Configuration(robot.model, robot.data, robot.q0)
-viz.display(configuration.q)
+
 T_pred = 100e-3  # (s)
 T_control = 100e-3  # (s)
 simulation_time = 10  # (s)
@@ -147,6 +147,7 @@ def move(trajectory_type, debug=False, store=False, perturbations=None, filename
     zk_min_x, zk_max_x, zk_min_y, zk_max_y, theta_ref = step_planner.footsteps_to_array(
         0, simulation_time, T_control
     )
+
     t = np.arange(0, simulation_time, T_control)
     zk_ref_x = (zk_min_x + zk_max_x) / 2
     zk_ref_y = (zk_min_y + zk_max_y) / 2
@@ -185,6 +186,9 @@ def move(trajectory_type, debug=False, store=False, perturbations=None, filename
     T = T_pred  # Used for intermediate visualizations
     n_iterations = int(simulation_time / T_control)
 
+
+    # Running the script like the commented area allows for separation between trajectory
+    # planning and inverse kinematics
     # Run the MPC
     # com_x, com_y, cop_x, cop_y = [], [], [], []
 
@@ -206,11 +210,50 @@ def move(trajectory_type, debug=False, store=False, perturbations=None, filename
     #     # Run the MPC iteration and update the robot state
     #     controller.MPC_iteration(i, N, T, file=None)
 
-    com_x, com_y, cop_x, cop_y = controller.run_MPC(filename=filename)
-    # print(len(com_x))
+    cop_x, com_x, cop_y, com_y = controller.run_MPC(filename=filename)
 
-    seen = []
-    res = []
+    # Plot the results
+    plt.plot(t, cop_x, label="cop")
+    plt.plot(t, com_x, label="com")
+    plt.plot(t, zk_min_x, linewidth=0.7)
+    plt.plot(t, zk_max_x, linewidth=0.7)
+    plt.title("x movement")
+    plt.xlabel("time (s)")
+    plt.ylabel("x (m)")
+    # plt.ylim(0,2)
+    plt.legend()
+    plt.show()
+
+    plt.plot(t, cop_y, label="cop")
+    plt.plot(t, com_y, label="com")
+    plt.plot(t, zk_min_y, linewidth=0.7)
+    plt.plot(t, zk_max_y, linewidth=0.7)
+    # plt.ylim((-0.8, 0.8))
+    plt.title("y movement")
+    plt.xlabel("time (s)")
+    plt.ylabel("x (m)")
+    plt.legend()
+    plt.show()
+
+    fig, ax = plt.subplots()
+    ax.plot(cop_x, cop_y, label="cop", color="green")
+    ax.plot(com_x, com_y, label="com", color="red")
+    # Plot footsteps
+    plot_foot_steps(ax, (zk_min_x + zk_max_x) / 2, (zk_min_y + zk_max_y) / 2, theta_ref, foot_dimensions, spacing[1])
+    # Display the plot
+    plt.legend()
+    ax.set_xlabel("x(m)")
+    ax.set_ylabel("y(m)")
+    # ax.set_ylim((-0.03, 0.03))
+    plt.title("Trajectory of robot")
+    plt.show()
+
+    # Visualize the results with pink
+
+    viz.initViewer(open=True)
+    viz.loadViewerModel()
+    viz.display(configuration.q)
+
     left_foot = robot_mpc.offline_left_foot_trajectory
     right_foot = robot_mpc.offline_right_foot_trajectory
     com = robot_mpc.offline_com_trajectory
@@ -231,13 +274,13 @@ def move(trajectory_type, debug=False, store=False, perturbations=None, filename
     left_foot_orientation = [left_foot[i[0]][-1] for i in left_indices]
     right_foot_orientation = [right_foot[i[0]][-1] for i in right_indices]
 
-    print(left_foot_orientation)
-    print(right_foot_orientation)
-
-    print("left foot ->", left_foot_unique)
-    print("right foot ->", right_foot_unique)
-    print("com left ->", corresp_com_left)
-    print("com right -> ", corresp_com_right)
+    # print(left_foot_orientation)
+    # print(right_foot_orientation)
+    #
+    # print("left foot ->", left_foot_unique)
+    # print("right foot ->", right_foot_unique)
+    # print("com left ->", corresp_com_left)
+    # print("com right -> ", corresp_com_right)
 
     # define the tasks
     left_foot_task = FrameTask("l_ankle", position_cost=100.0, orientation_cost=1.0)
@@ -359,7 +402,7 @@ def move(trajectory_type, debug=False, store=False, perturbations=None, filename
 
 
 def main():
-    trajectory_type = "forward"
+    trajectory_type = input("Choose a trajectory type from : <forward>, <upwards>, <upwards_turning>, <circular>: ")
     move(trajectory_type, debug=False)
 
 
